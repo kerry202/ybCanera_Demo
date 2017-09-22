ea069f49b23fa5ba1cab6287e251de5b0fac3769
PRIVATE\Administrator <903023560@qq.com>
2017-09-22 14:49:19 +0800

    首次提交.关键代码在是libary

---
+++ cameralibrary/src/main/java/camera/yb/com/ImagePickerProvider.java
@@ -0,0 +1,14 @@
+package camera.yb.com;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * 自定义一个Provider，以免和引入的项目的provider冲突
+ *
+ * author : yangbo（yangbo Github地址：https://github.com/yb801925
+ * Email: liushilin520@foxmail.com
+ * Date: 2017-03-17  16:10
+ */
+
+public class ImagePickerProvider extends FileProvider {
+}
---
+++ cameralibrary/src/main/java/camera/yb/com/view/ViewPagerFixed.java
@@ -0,0 +1,46 @@
+package camera.yb.com.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * ================================================
+ * 作    者：yangbo（yangbo Github地址：https://github.com/yb801925
+ * 版    本：1.0
+ * 创建日期：2016/5/19
+ * 描    述：修复图片在ViewPager控件中缩放报错的BUG
+ * 修订历史：
+ * ================================================
+ */
+public class ViewPagerFixed extends ViewPager {
+
+    public ViewPagerFixed(Context context) {
+        super(context);
+    }
+
+    public ViewPagerFixed(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
